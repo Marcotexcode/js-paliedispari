c5a65f5182c4fbc27ecfe63845d7d05a656edf37
Marco Tessitore <marcotexcode@gmail.com>
2021-06-07 15:21:58 +0200

    add: esercizio 2

---
+++ esercizio2/js/script.js
@@ -0,0 +1,16 @@
+
+
+
+
+// Palidroma Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+// 1- Inserire parola
+
+var parola = prompt('inserisci una parola');
+
+
+
+
+
