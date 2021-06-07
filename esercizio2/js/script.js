885736caa6221725cab49158b34eea30aa5eeb1e
Marco Tessitore <marcotexcode@gmail.com>
2021-06-07 15:31:47 +0200

    ad:esercizio 2

--- esercizio2/js/script.js
+++ esercizio2/js/script.js
@@ -6,11 +6,25 @@
 
 
 
-// 1- Inserire parola
+//  1- Inserire parola utente
 
 var parola = prompt('inserisci una parola');
 
+// 2- invertire parola con funzione
 
+var parolaInversa = invertiParola(parola);
 
+function invertiParola(str){
+    var parInversa = str.split('').reverse().join('');  
+    return parInversa;
+}
 
+console.log(parolaInversa);
 
+// 3 - controllare se e palindroma
+
+if (parola === parolaInversa) {
+    console.log('E palindroma');
+}else {
+    console.log('Non e palindroma');
+}
